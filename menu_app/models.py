925bf6c8af961b28e67fa6ef1d90f4d434865e29
unknown <gulyamov640@gmail.com>
2023-11-13 18:15:34 +0500

    first commit

--- menu_app/models.py
+++ menu_app/models.py
@@ -3,32 +3,36 @@ from django.contrib.auth.models import User
 from django.db.models import SET_NULL, PROTECT, CASCADE
 
 
-
 class Basemodel(models.Model):
     created_time = models.DateTimeField(auto_now_add=True, editable=False, null=True)
     update_time = models.DateTimeField(auto_now=True, editable=False, null=True)
-    created_by = models.ForeignKey(User, SET_NULL, null=True, blank=True, related_name='created_%(model_name)ss')
-    update_by = models.ForeignKey(User, SET_NULL, null=True, blank=True, related_name='updated_%(model_name)ss')
+    created_by = models.ForeignKey(
+        User, SET_NULL, null=True, blank=True, related_name="created_%(model_name)ss"
+    )
+    update_by = models.ForeignKey(
+        User, SET_NULL, null=True, blank=True, related_name="updated_%(model_name)ss"
+    )
 
     class Meta:
-       abstract = True
-       ordering = ('id',)
+        abstract = True
+        ordering = ("id",)
+
 
 class Restaurant(Basemodel):
-    name = models.CharField(max_length=225, null=True, blank= False)
+    name = models.CharField(max_length=225, null=True, blank=False)
     adress = models.CharField(max_length=225)
-    photo = models.FileField(upload_to='media', null=True, blank=False)
-    logo = models.FileField(upload_to='logo', null=True, blank=False)
+    photo = models.FileField(upload_to="media", null=True, blank=False)
+    logo = models.FileField(upload_to="logo", null=True, blank=False)
 
     def __str__(self):
         return self.name
+
 
 class Category(Basemodel):
     name = models.CharField(max_length=225)
 
     def __str__(self):
         return self.name
-
 
 
 class Menu(Basemodel):
@@ -41,7 +45,4 @@ class Menu(Basemodel):
     restaurant = models.ForeignKey(Restaurant, CASCADE, null=True, blank=True)
 
     def __str__(self):
-        return f'{self.name} in {self.restaurant.name}'
-
-
-
+        return f"{self.name}"
--- menu_app/views.py
+++ menu_app/views.py
@@ -6,18 +6,18 @@ from .models import *
 
 
 class restaurant(ModelViewSet):
-    queryset = Restaurant
+    queryset = Restaurant.objects.all()
     parser_classes = [MultiPartParser, FormParser]
     serializer_class = RestaurantSerializers
 
 
 class category(ModelViewSet):
-    queryset = Category
+    queryset = Category.objects.all()
     parser_classes = [MultiPartParser, FormParser]
     serializer_class = CategorySerializer
 
 
 class menu(ModelViewSet):
-    queryset = Menu
+    queryset = Menu.objects.all()
     parser_classes = [MultiPartParser, FormParser]
     serializer_class = MenuSerializer
